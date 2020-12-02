a78f06d843f382d673f236b01db03bba11b7649c
风---自由 <996442717qqcom@gmail.com>
2020-12-02 15:06:22 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,7 +1,6 @@
 return {
     Component = require("fecs.component"),
     Entity = require("fecs.entity"),
-    Global = require("fecs.global"),
     System = require("fecs.system"),
     World = require("fecs.world")
 }
